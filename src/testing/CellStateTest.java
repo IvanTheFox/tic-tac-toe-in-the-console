70cc3ef782e01666fb81421dd2bce4c9f19cf4c8
IvanTheFox <ivan.shepelev.06@inbox.ru>
2025-12-05 16:51:17 +0300

    added a folder for java test files and separated it from the application files

---
+++ src/testing/CellStateTest.java
@@ -0,0 +1,10 @@
+package testing;
+import app.CellState;
+
+public class CellStateTest {
+    public static void main(String[] args) {
+        CellState cs = CellState.Cross;
+        System.out.println(cs);
+        System.out.println(cs.getValue());
+    }
+}
