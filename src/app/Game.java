8b0cc2df94bfa2c92925780eb2cb451ef8b653e2
Vlad <ytvctyg@mail.ru>
2025-12-06 18:40:41 +0300

    Исправил ошибки, добавил тесты

--- src/app/Game.java
+++ src/app/Game.java
@@ -6,6 +6,7 @@ public class Game {
 
     public void Reset(){
         field.Reset();
+        isCrossNext = true;
     }
 
     public Game(){
@@ -30,58 +31,66 @@ public class Game {
         CellState oldState = field.GetState(x-1,y-1);
         if (oldState == CellState.Empty){
             field.Mark(x-1,y-1,newState);
+            isCrossNext = !isCrossNext;
+
         }
         else {
             throw new Exception("Эта клетка уже занята!");
         }
+
     }
 
     public CellState CheckWin(){
         // Проверка диагоналей для крестика
         boolean crossWinToRight = true;
         boolean crossWinToLeft = true;
-        for (int i = 1; i < 4; i++){
-            crossWinToRight = crossWinToRight & (field.GetState(i-1,i-1) == CellState.Cross);
-            crossWinToLeft = crossWinToLeft & (field.GetState(4-i-1,i-1) == CellState.Cross);
+        for (int i = 0; i < 3; i++){
+            crossWinToRight = crossWinToRight & (field.GetState(i,i) == CellState.Cross);
+            crossWinToLeft = crossWinToLeft & (field.GetState(2-i,i) == CellState.Cross);
         }
         if (crossWinToLeft || crossWinToRight){
             return CellState.Cross;
         }
         // Проверка вертикалей и горизонталей для крестика
-        boolean crossWinOnRows = true;
-        boolean crossWinOnCols = true;
-        for (int col = 1; col < 4; col++) {
-            for (int row = 1; row < 4; row++) {
-                crossWinOnRows = crossWinOnRows & (field.GetState(row-1, col-1) == CellState.Cross);
-                crossWinOnCols = crossWinOnRows & (field.GetState(col-1, row-1) == CellState.Cross);
+        for (int i = 0; i < 3; i++) {
+            // Проверка строки i
+            if (field.GetState(i, 0) == CellState.Cross &&
+                    field.GetState(i, 1) == CellState.Cross &&
+                    field.GetState(i, 2) == CellState.Cross) {
+                return CellState.Cross;
             }
-        }
-        if (crossWinOnCols || crossWinOnRows){
-            return CellState.Cross;
+            // Проверка столбца i
+            if (field.GetState(0, i) == CellState.Cross &&
+                    field.GetState(1, i) == CellState.Cross &&
+                    field.GetState(2, i) == CellState.Cross) {
+                return CellState.Cross;
+            }
         }
 
         // Проверка диагоналей для нолика
         boolean circleWinToRight = true;
         boolean circleWinToLeft = true;
-        for (int i = 1; i < 4; i++){
-            circleWinToRight = circleWinToRight & (field.GetState(i-1,i-1) == CellState.Circle);
-            circleWinToLeft = circleWinToLeft & (field.GetState(4-i-1,i-1) == CellState.Circle);
+        for (int i = 0; i < 3; i++){
+            circleWinToRight = circleWinToRight & (field.GetState(i,i) == CellState.Circle);
+            circleWinToLeft = circleWinToLeft & (field.GetState(2-i,i) == CellState.Circle);
         }
         if (circleWinToLeft || circleWinToRight){
             return CellState.Circle;
         }
         // Проверка вертикалей и горизонталей для нолика
-        boolean circleWinOnRows = true;
-        boolean circleWinOnCols = true;
-        for (int col = 1; col < 4; col++) {
-            for (int row = 1; row < 4; row++) {
-                circleWinOnRows = circleWinOnRows & (field.GetState(row-1, col-1) == CellState.Circle);
-                circleWinOnCols = circleWinOnRows & (field.GetState(col-1, row-1) == CellState.Circle);
+        for (int i = 0; i < 3; i++) {
+            if (field.GetState(i, 0) == CellState.Circle &&
+                    field.GetState(i, 1) == CellState.Circle &&
+                    field.GetState(i, 2) == CellState.Circle) {
+                return CellState.Circle;
+            }
+            if (field.GetState(0, i) == CellState.Circle &&
+                    field.GetState(1, i) == CellState.Circle &&
+                    field.GetState(2, i) == CellState.Circle) {
+                return CellState.Circle;
             }
         }
-        if (circleWinOnCols || circleWinOnRows){
-            return CellState.Circle;
-        }
+
         // Случай ничьи
         return CellState.Empty;
     }
--- src/testing/GameTest.java
+++ src/testing/GameTest.java
@@ -5,17 +5,68 @@ import app.GameField;
 
 public class GameTest {
 
-    private static Game game;
-
+    private final static Game game = new Game();
     public static void main(String args[]){
-        game = new Game();
         try {
+            // Тест1. Победа за крестик по верхней горизонтали
             game.PlaceMark(1, 1);
             game.PlaceMark(1,2);
             game.PlaceMark(2,1);
             game.PlaceMark(1,3);
             game.PlaceMark(3,1);
-            System.out.println(game.CheckWin());
+            System.out.println("Тест1: " + game.CheckWin());
+
+            // Тест2. Победа за нолик по средней горизонтали
+            game.Reset();
+            game.PlaceMark(3,3);
+            game.PlaceMark(1,2);
+            game.PlaceMark(2,3);
+            game.PlaceMark(2,2);
+            game.PlaceMark(3,1);
+            game.PlaceMark(3,2);
+            System.out.println("Тест2: " + game.CheckWin());
+
+            // Тест3. Победа за нолик по правой вертикали
+            game.Reset();
+            game.PlaceMark(2,1);
+            game.PlaceMark(3,1);
+            game.PlaceMark(1,2);
+            game.PlaceMark(3,2);
+            game.PlaceMark(1,1);
+            game.PlaceMark(3,3);
+            System.out.println("Тест3: " + game.CheckWin());
+
+            // Тест4. Победа за крестик по диагонали (из верхнего правого угла до левого нижнего)
+            game.Reset();
+            game.PlaceMark(3,1);
+            game.PlaceMark(1,1);
+            game.PlaceMark(2,2);
+            game.PlaceMark(3,2);
+            game.PlaceMark(1,3);
+            System.out.println("Тест4: " + game.CheckWin());
+
+            // Тест5. Победа за нолик по диагонали (из верхнего левого угла до правого нижнего)
+            game.Reset();
+            game.PlaceMark(2,1);
+            game.PlaceMark(1,1);
+            game.PlaceMark(3,1);
+            game.PlaceMark(2,2);
+            game.PlaceMark(2,3);
+            game.PlaceMark(3,3);
+            System.out.println("Тест5: " + game.CheckWin());
+
+            // Тест6. Выход за пределы поля
+            game.Reset();
+            System.out.print("Тест6: ");
+            game.PlaceMark(2,4);
+            System.out.print("\n");
+
+            // Тест7. Ход в клетку, где уже есть фигура
+            game.Reset();
+            System.out.print("Тест7: ");
+            game.PlaceMark(2,2);
+            game.PlaceMark(2,2);
+            System.out.print("\n");
         }
         catch (Exception ex){
             System.err.println("Error: " + ex.getMessage());
