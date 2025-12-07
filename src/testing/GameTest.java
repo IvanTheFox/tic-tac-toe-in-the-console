f825ff054bab31873e232457d20384f2f5046b6c
IvanTheFox <ivan.shepelev.06@inbox.ru>
2025-12-07 21:29:40 +0300

    Added a way to check if the game field is full or not

--- src/testing/GameTest.java
+++ src/testing/GameTest.java
@@ -58,15 +58,29 @@ public class GameTest {
             // Тест6. Выход за пределы поля
             game.Reset();
             System.out.print("Тест6: ");
-            game.PlaceMark(2,4);
+            // game.PlaceMark(2,4);
             System.out.print("\n");
 
             // Тест7. Ход в клетку, где уже есть фигура
             game.Reset();
             System.out.print("Тест7: ");
             game.PlaceMark(2,2);
-            game.PlaceMark(2,2);
+            // game.PlaceMark(2,2);
             System.out.print("\n");
+
+            // Тест8. Проверка заполненности поля (поле не заполнено)
+            game.Reset();
+            System.out.print("Тест8: ");
+            System.out.println(game.IsFieldFull());
+
+            // Тест9. Проверка заполнения поля (поле заполнено)
+            System.out.print("Тест 9: ");
+            for (int x = 1; x < 4; x++) {
+                for (int y = 1; y < 4; y++) {
+                    game.PlaceMark(x, y);
+                }
+            }
+            System.out.println(game.IsFieldFull());
         }
         catch (Exception ex){
             System.err.println("Error: " + ex.getMessage());
