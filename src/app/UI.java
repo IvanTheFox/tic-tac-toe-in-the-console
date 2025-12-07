b6686776f882346ab651a1a232b3464388db7d25
Pizzaguyman <mihan02032006@mail.ru>
2025-12-07 18:36:07 +0300

    UI methods realised

--- src/app/UI.java
+++ src/app/UI.java
@@ -3,10 +3,10 @@ package app;
 import java.util.Scanner;
 
 public class UI {
-    //private Game game;
+    private Game game;
     private Scanner scanner;
     public UI(){
-        //game = new Game();
+        game = new Game();
     }
     public String GetLine(String text) {
         try {
@@ -27,35 +27,22 @@ public class UI {
     }
     public void ParseCommand(String input) {
         try {
-            String input1 = input.split(input)[0];
-            switch (input1) {
-                case "start":
-                    switch (input) {
-                        default:
-                            if (input.split(" ")[0]=="place"){
-                                if (Integer.parseInt(input.split(" ")[1])<1||Integer.parseInt(input.split(" ")[1])>3||Integer.parseInt(input.split(" ")[2])<1||Integer.parseInt(input.split(" ")[2])>3){
-
-                                } else {
-
-                                }
-                            }
-                            break;
-                    }
-                    break;
-                
+            String input1 = input.split(" ")[0];
+            switch (input1) {               
                 case "exit":
                     break;
 
                 case "reset":
+                    game.Reset();
                     break;
 
                 case "place":
                     ShowField();
-                    //game.PlaceMark(Integer.parseInt(input.split(" ")[1],Integer.parseInt(input.split(" ")[2]);
-
+                    game.PlaceMark(Integer.parseInt(input.split(" ")[1]),Integer.parseInt(input.split(" ")[2]));
                     break;
 
                 default:
+                    ParseCommand(input);
                     break;
         }
         } catch (Exception e) {
@@ -64,10 +51,29 @@ public class UI {
         
     }
     public void ShowField() {
-
+        GameField field = game.field;
+        System.out.println(field.GetState(1,3).toString()+"│"+field.GetState(2,3).toString()+"│"+field.GetState(3,3).toString());
+        System.out.println("─┼─┼─");
+        System.out.println(field.GetState(1,2).toString()+"│"+field.GetState(2,2).toString()+"│"+field.GetState(3,2).toString());
+        System.out.println("─┼─┼─");
+        System.out.println(field.GetState(1,1).toString()+"│"+field.GetState(2,1).toString()+"│"+field.GetState(3,1).toString());
+        System.out.println("");
     }
     public void AnnounceWin(CellState winner){
-
+        switch (winner) {
+            case CellState.Cross:
+                System.out.println("Crosses won!");
+                break;
+            case CellState.Circle:
+                System.out.println("Circles won!");
+                break;
+            case CellState.Empty:
+                System.out.println("Draw!");
+                break;
+            default:
+                break;
+        }
+        System.out.println();
     }
     public void AppStart() {
         String input = GetLine("Enter place, reset or exit");
